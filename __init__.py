c318c3a4be0d6596568e90b06314ef5077e1c364
Lead <YGzaIHzf8pL4wc69Mry0EJdO@gmail.com>
2015-02-04 22:10:21 -0500

    PEP8 compliant (yeah new pylint install in IDE), killed some JS baggage missed earlier, more power added to comments.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 #!/usr/bin/env python
+"""color-proximity module"""
--- tests.py
+++ tests.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
-from color_proximity import color_proximity
+"""Tests for color proximity"""
+from ColorProximity import ColorProximity
 
-cp = color_proximity()
+CP = ColorProximity()
 
 # test as tuples
-assert str(cp.proximity((255,255,255), (255,255,254)))==str(0.507421915175), \
-    'Fail, test 1'
-assert str(cp.proximity((255,255,255), (255,255,255)))==str(0.0), \
+assert str(CP.proximity((255, 255, 255), (255, 255, 254))) == \
+    str(0.507421915175), 'Fail, test 1'
+assert str(CP.proximity((255, 255, 255), (255, 255, 255))) == str(0.0), \
     'Fail, test 2'
-assert str(cp.proximity((255,255,255), (0,0,0)))==str(100.000000625), \
+assert str(CP.proximity((255, 255, 255), (0, 0, 0))) == str(100.000000625), \
     'Fail, test 3'
-assert str(cp.proximity((255,255,255), (1,1,1)))==str(99.7260006267), \
+assert str(CP.proximity((255, 255, 255), (1, 1, 1))) == str(99.7260006267), \
     'Fail, test 4'
 
 # test as list
-assert str(cp.proximity([255,255,255], [255,255,254]))==str(0.507421915175), \
-    'Fail, test 5'
-assert str(cp.proximity([255,255,255], [255,255,255]))==str(0.0), \
+assert str(CP.proximity([255, 255, 255], [255, 255, 254])) == \
+    str(0.507421915175), 'Fail, test 5'
+assert str(CP.proximity([255, 255, 255], [255, 255, 255])) == str(0.0), \
     'Fail, test 6'
-assert str(cp.proximity([255,255,255], [0,0,0]))==str(100.000000625), \
+assert str(CP.proximity([255, 255, 255], [0, 0, 0])) == str(100.000000625), \
     'Fail, test 7'
-assert str(cp.proximity([255,255,255], [1,1,1]))==str(99.7260006267), \
+assert str(CP.proximity([255, 255, 255], [1, 1, 1])) == str(99.7260006267), \
     'Fail, test 8'
 
 # tuple w/alpha value
-assert str(cp.proximity((255,255,255,20), (255,255,254,30)))== \
+assert str(CP.proximity((255, 255, 255, 20), (255, 255, 254, 30))) == \
     str(0.507421915175), 'Fail, test 9'
 
 # list w/alpha value
-assert str(cp.proximity([255,255,255,20], [255,255,254,30]))== \
+assert str(CP.proximity([255, 255, 255, 20], [255, 255, 254, 30])) == \
     str(0.507421915175), 'Fail, test 10'
 
 # mixed tuple and list w/alpha value
-assert str(cp.proximity([255,255,255,20], (255,255,254,30)))== \
+assert str(CP.proximity([255, 255, 255, 20], (255, 255, 254, 30))) == \
     str(0.507421915175), 'Fail, test 11'
 
-print 'pass';
+print 'pass'
